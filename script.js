76f24a1f6a521a813c13d7e590744bf780154ecb
Gustavo <gustavodnts07u@example.com>
2024-10-15 19:18:55 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')) {
+ img.setAttribute('src', './assets/fotoluz.png')
+    } else {
+        img.setAttribute("src", "./assets/419051642_694914926090289_5500336131213445145_n.jpg")
+
+    }
+
+
+}
+
